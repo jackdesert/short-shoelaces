2bc707d5f77f8220496a759aabb0d12b1c42cfc1
Jack Desert <jackdesert@gmail.com>
2016-04-02 10:54:06 -0700

    Flashy and Concise text
    Also improved watcher script to run plimc on startup
    so no need to re-save source file after starting watcher.

--- watcher.sh
+++ watcher.sh
@@ -1,3 +1,24 @@
 #! /bin/bash
 
-watchmedo shell-command --pattern '*.slim' --command 'plimc index.slim > generatedndex.html; echo "${watch_src_path}"'
+echo 'Starting watcher'
+
+# Run COMMAND on startup, and notify if changed
+
+GENERATED_FILE=generated/index.html
+
+if [ -e $GENERATED_FILE ]; then
+  MD5_BEFORE=`md5sum $GENERATED_FILE`
+else
+  MD5_BEFORE='some-random-string-lalala'
+fi
+
+plimc index.slim > generated/index.html
+
+MD5_AFTER=`md5sum $GENERATED_FILE`
+
+if [ "$MD5_BEFORE" != "$MD5_AFTER" ];then
+  echo 'changed index.slim'
+fi
+
+# Start watcher to check for future changes
+watchmedo shell-command --pattern '*.slim' --command 'plimc index.slim > generated/index.html; echo "changed ${watch_src_path}"'
