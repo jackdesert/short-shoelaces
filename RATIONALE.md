45d5c3cc5d999bddae97219fb79ed692db256058
Jack Desert <jackdesert@gmail.com>
2016-04-02 19:54:51 -0700

    Added images

---
+++ RATIONALE.md
@@ -0,0 +1,11 @@
+Rationale
+=========
+
+Q: Why plim?
+A: Wanted this to be a static website, but did not want to edit html directly.
+   Staticmatic is a great alternative in Ruby that uses haml and sass, but
+   wanted something in python. Slim is similar to haml, and has a python
+   implementation---plim
+
+Q: Why a static website?
+A: Uses fewer server resources to host it.
--- bin/watcher.sh
+++ bin/watcher.sh
@@ -4,7 +4,7 @@ echo 'Starting watcher'
 
 # Run COMMAND on startup, and notify if changed
 
-GENERATED_FILE=generated/index.html
+GENERATED_FILE=index.html
 
 if [ -e $GENERATED_FILE ]; then
   MD5_BEFORE=`md5sum $GENERATED_FILE`
@@ -12,13 +12,13 @@ else
   MD5_BEFORE='some-random-string-lalala'
 fi
 
-plimc index.slim > generated/index.html
+plimc src/index.slim > index.html
 
 MD5_AFTER=`md5sum $GENERATED_FILE`
 
 if [ "$MD5_BEFORE" != "$MD5_AFTER" ];then
-  echo 'changed index.slim'
+  echo 'changed src/index.slim'
 fi
 
 # Start watcher to check for future changes
-watchmedo shell-command --pattern '*.slim' --command 'plimc index.slim > generated/index.html; echo "changed ${watch_src_path}"'
+watchmedo shell-command --pattern 'src/index.slim' --command 'plimc src/index.slim > index.html; echo "changed ${watch_src_path}"'
